c5370683f2113f58f107574641117240acbd9c45
stanislav.tkachev <tkachsta@gmail.com>
2023-02-05 21:31:52 +0300

    Refactoring JPA queries for LemmaRepository

--- src/main/java/searchengine/model/lemma/LemmaRepository.java
+++ src/main/java/searchengine/model/lemma/LemmaRepository.java
@@ -6,27 +6,22 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import searchengine.model.site.SiteEntity;
-import java.util.Collection;
-import java.util.List;
 import java.util.Optional;
 
 @Repository
 @Transactional
 public interface LemmaRepository extends CrudRepository <LemmaEntity, Integer> {
-
-    @Query("SELECT COUNT(*) FROM LemmaEntity")
-    int findCount();
-    @Query("SELECT COUNT(*) FROM LemmaEntity l WHERE l.site = :site")
-    int findCountBySite(@Param("site") SiteEntity site);
+    int countAllBy();
+    int countAllBySite(SiteEntity site);
+    @Modifying
+    void deleteAllBySite(SiteEntity site);
     @Query("SELECT COUNT(DISTINCT site) FROM LemmaEntity")
     int findUniqNumberOfSites();
     @Query("SELECT SUM(frequency) FROM LemmaEntity")
     int totalSumOfFrequency();
     @Query("SELECT SUM(l.frequency) FROM LemmaEntity l WHERE l.lemma = :lemma")
     Optional<Integer> sumOfLemmaFrequency(@Param("lemma") String lemma);
-    @Modifying
-    @Query("DELETE FROM LemmaEntity l WHERE l.site = :site")
-    void removeAllBySite(@Param("site") SiteEntity site);
+
 
 
 
