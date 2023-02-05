43ea706e270ef783398daa2d664896ab51ff3d99
stanislav.tkachev <tkachsta@gmail.com>
2023-02-05 21:30:29 +0300

    Refactoring JPA queries for SiteRepository

--- src/main/java/searchengine/model/site/SiteRepository.java
+++ src/main/java/searchengine/model/site/SiteRepository.java
@@ -1,8 +1,6 @@
 package searchengine.model.site;
 import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
@@ -10,16 +8,11 @@ import java.util.List;
 @Repository
 @Transactional
 public interface SiteRepository extends CrudRepository<SiteEntity, Integer> {
-    @Query("SELECT COUNT(*) FROM SiteEntity")
-    int findCount();
-    @Query("SELECT s FROM SiteEntity s WHERE s.status = :status")
-    Iterable<SiteEntity> findByStatus(@Param("status") IndexingStatus status);
-    @Query("SELECT s FROM SiteEntity s")
-    List<SiteEntity> findAllSites();
-    @Query("SELECT s FROM SiteEntity s WHERE s.url = :url")
-    SiteEntity findByUrl(@Param("url") String url);
+    int countAllBy();
+    List<SiteEntity> findAllByStatus(IndexingStatus status);
+    List<SiteEntity> findAllBy();
+    SiteEntity findByUrl(String url);
     @Modifying
-    @Query("DELETE FROM SiteEntity s WHERE s.url = :url")
-    void removeAllByUrl(@Param("url") String url);
+    void deleteAllByUrl(String url);
 
 }
